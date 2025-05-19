fefedc658c3342b037e64e77c89703d591ca4fe2
Peterjoram37 <videohidden21@gmail.com>
2025-05-19 14:42:41 +0000

    main

---
+++ pair.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+__path = process.cwd();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+
+let server = require('./session-generator'); // au './qr' kama umeipa jina hilo
+let code = require('./pair'); // sasa pair.js yako
+
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+app.use('/qr', server);
+app.use('/code', code); // hii ni route ya pair.js yako
+
+app.use('/pair', async (req, res, next) => {
+  res.sendFile(__path + '/pair.html');
+});
+app.use('/', async (req, res, next) => {
+  res.sendFile(__path + '/main.html');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`
+Don't Forgot To Give Star
+
+ Server running on http://localhost:` + PORT)
+});
+
+module.exports = app;
